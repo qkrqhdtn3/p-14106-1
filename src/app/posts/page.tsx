26a9939a7bf3f7021f881f266b4fe7d7f4aa0099
qkrqhdtn3 <qkrqhdtn3@gmail.com>
2025-12-10 15:14:33 +0900

    13

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,13 +1,26 @@
 "use client";
 
+import { useEffect, useState } from "react";
+
 export default function Page() {
+  const [posts, setPosts] = useState<{ id: number; title: string }[]>([]);
+
+  useEffect(() => {
+    fetch("http://localhost:8080/api/v1/posts")
+      .then((res) => res.json())
+      .then((data) => setPosts(data));
+  }, []);
+
   return (
     <>
       <h1>글 목록</h1>
 
+      {posts.length == 0 && <div>로딩중...</div>}
+
       <ul>
-        <li>명언 1</li>
-        <li>명언 2</li>
+        {posts.map((post) => (
+          <li key={post.id}>{post.title}</li>
+        ))}
       </ul>
     </>
   );
